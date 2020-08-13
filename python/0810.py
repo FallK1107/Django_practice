16203636b928a784d2bbe8b7a6ad589f30b70200
FallK1107 <jprkdmf@gmail.com>
2020-08-13 18:23:52 +0900

    first commit

---
+++ python/0810.py
@@ -0,0 +1,40 @@
+# print("Hello World")
+
+# print("멋사")
+# print(1+1)
+
+# name = "김가을"
+# print("나의 이름은", name)
+
+# number1 = input()
+# number2 = input()
+# print(number1 + number2)
+
+# number3 = int(input("첫번째 숫자를 입력하세요 : "))
+# number4 = int(input("두번째 숫자를 입력하세요 : "))
+# print(number3 + number4)
+
+# print(1, end = " ")
+# print(2, end = " ")
+# print(3, end = " ")
+
+# head = "멋쟁이"
+# tail = "사자처럼"
+# print(head + tail)
+# print(head*3)
+
+# str = "계명대 멋사 8기"
+# print(len(str))
+
+# a = "멋쟁이 사자처럼"
+# print(a[4:-2]) 
+# print(a[:])
+
+name = "멋사"
+print("나는 %s 동아리에 가입되어 있다." %name)
+
+num = 3
+print("나는 사과 %d개를 가지고 있다." %num)
+
+num = 3
+print("나는 사과 {0}개를 가지고 있다." .format(num))
