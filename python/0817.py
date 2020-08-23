7975306a8e8946752ceadc203b4b0bfcfd2ed1dd
FallK1107 <69625223+FallK1107@users.noreply.github.com>
2020-08-23 21:58:13 +0900

    멋사 파이썬 강의

---
+++ python/0817.py
@@ -0,0 +1,54 @@
+# a = int(input("점수를 입력해주세요"))
+# if(a>=90):
+#     print("A")
+# elif(a>=80):
+#     print("B")
+# elif(a>=70):
+#     print("C")
+# elif(a>=60):
+#     print("D")
+# else:
+#     print("F") 
+
+# a = 0
+# while(True):
+#     if(a%2==0):
+#         a = a+1
+#         continue
+
+# test_list = ['one', 'two', 'three']
+# for i in test_list:
+#     print(i)
+
+# a = [(1,2),(3,4),(5,6)]
+# for (first,last) in a:
+#     print(first," ",last)
+
+# marks = [90, 25, 67, 45, 80]
+# num = 0
+# for x in marks:
+#     num = num + 1
+#     if(x>=60): continue
+#         print("{0}번 학생 축하합니다. 합격입니다." .format(num))
+#     else:
+#         print("불합격")
+
+# add = 0
+# for i in range(1,11):
+#     add +=i
+# print(add)
+
+# coffee = 10
+
+# while(coffee>=0):
+#     money = int(input("돈을 넣어주세요"))
+#     if(money>300):
+#         print("거스름돈 %d를 주고 커피를 줍니다." %(money-300))
+#         coffee = coffee -1
+#     elif(money==300):
+#         print("커피를 줍니다")
+#         coffee = coffee -1
+#     else:
+#         print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+#         print("남은 커피의 양은 %d개 입니다." %coffee)
+#         break
